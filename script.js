0e359490c99ac66a4210c68f471ce0de3e42bed7
ryangehan <102697120+ryangehan@users.noreply.github.com>
2023-03-16 17:20:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,170 @@
-const playButton = document.getElementById("play-button");
-const resultParagraph = document.getElementById("result");
-const moneyParagraph = document.getElementById("money");
+const suits = ["hearts", "diamonds", "clubs", "spades"];
+const values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+const playerHandElement = document.getElementById("player-hand");
+const dealerHandElement = document.getElementById("dealer-hand");
+const playerScoreElement = document.getElementById("player-score");
+const dealerScoreElement = document.getElementById("dealer-score");
+const hitButton = document.getElementById("hit-button");
+const standButton = document.getElementById("stand-button");
+const newGameButton = document.getElementById("new-game-button");
 
-let money = 100;
+let deck = [];
+let playerHand = [];
+let dealerHand = [];
+let playerScore = 0;
+let dealerScore = 0;
+let gameOver = false;
 
-playButton.addEventListener("click", () => {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  
-  if (randomNumber <= 40) {
-    resultParagraph.textContent = "You win $50!";
-    money += 50;
-  } else if (randomNumber <= 80) {
-    resultParagraph.textContent = "You win $25!";
-    money += 25;
-  } else if (randomNumber <= 95) {
-    resultParagraph.textContent = "You win $10!";
-    money += 10;
-  } else {
-    resultParagraph.textContent = "Sorry, you lose $10.";
-    money -= 10;
-  }
-  
-  moneyParagraph.textContent = `You have $${money}.`;
+newGameButton.addEventListener("click", () => {
+  deck = createDeck();
+  playerHand = [];
+  dealerHand = [];
+  playerScore = 0;
+  dealerScore = 0;
+  gameOver = false;
+  hitButton.disabled = false;
+  standButton.disabled = false;
+  newGameButton.disabled = true;
+  playerHandElement.innerHTML = "";
+    dealerHandElement.innerHTML = "";
+  playerScoreElement.textContent = "0";
+  dealerScoreElement.textContent = "0";
+  dealCards();
 });
+
+hitButton.addEventListener("click", () => {
+  if (!gameOver) {
+    playerHand.push(getCard());
+    addCardToHand(playerHandElement, playerHand[playerHand.length - 1]);
+    playerScore = calculateScore(playerHand);
+    playerScoreElement.textContent = playerScore.toString();
+    if (playerScore > 21) {
+      gameOver = true;
+      endGame();
+    }
+  }
+});
+
+standButton.addEventListener("click", () => {
+  if (!gameOver) {
+    while (dealerScore < 17) {
+      dealerHand.push(getCard());
+      addCardToHand(dealerHandElement, dealerHand[dealerHand.length - 1]);
+      dealerScore = calculateScore(dealerHand);
+      dealerScoreElement.textContent = dealerScore.toString();
+    }
+    gameOver = true;
+    endGame();
+  }
+});
+
+function createDeck() {
+  let deck = [];
+  for (let suit of suits) {
+    for (let value of values) {
+      deck.push({ suit, value });
+    }
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+function getCard() {
+  return deck.pop();
+}
+
+function addCardToHand(handElement, card) {
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+  const suitElement = document.createElement("p");
+  suitElement.textContent = card.suit;
+  const valueElement = document.createElement("p");
+  valueElement.textContent = card.value.toString();
+  cardElement.appendChild(suitElement);
+  cardElement.appendChild(valueElement);
+  handElement.appendChild(cardElement);
+}
+
+function calculateScore(hand) {
+  let score = 0;
+  let hasAce = false;
+  for (let card of hand) {
+    if (card.value === "Ace") {
+      hasAce = true;
+    }
+    score += getCardValue(card.value);
+  }
+  if (hasAce && score + 10 <= 21) {
+    score += 10;
+  }
+  return score;
+}
+
+function getCardValue(value) {
+  if (value === "Ace") {
+    return 1;
+  } else if (typeof value === "number") {
+    return value;
+  } else {
+    return 10;
+  }
+}
+
+function dealCards() {
+  playerHand.push(getCard());
+  addCardToHand(playerHandElement, playerHand[0]);
+  playerScore = calculateScore(playerHand);
+  playerScoreElement.textContent = playerScore.toString();
+
+  dealerHand.push(getCard());
+  addCardToHand(dealerHandElement, dealerHand[0]);
+  dealerScore = calculateScore(dealerHand);
+  dealerScoreElement.textContent = dealerScore.toString();
+
+  playerHand.push(getCard());
+  addCardToHand(playerHandElement, playerHand[1]);
+  playerScore = calculateScore(playerHand);
+  playerScoreElement.textContent = playerScore.toString();
+}
+
+function endGame() {
+  hitButton.disabled = true;
+  standButton.disabled = true;
+  newGameButton.disabled = false;
+  if (playerScore > 21) {
+    alert("You bust, dealer wins!");
+  } else if (dealerScore > 21) {
+    alert("Dealer busts, you win!");
+    } else if (playerScore > dealerScore) {
+    alert("You win!");
+  } else if (playerScore < dealerScore) {
+    alert("Dealer wins!");
+  } else {
+    alert("It's a tie!");
+  }
+}
+
+function resetGame() {
+  playerHand = [];
+  dealerHand = [];
+  playerScore = 0;
+  dealerScore = 0;
+  gameOver = false;
+  playerHandElement.innerHTML = "";
+  dealerHandElement.innerHTML = "";
+  playerScoreElement.textContent = "0";
+  dealerScoreElement.textContent = "0";
+  hitButton.disabled = false;
+  standButton.disabled = false;
+  newGameButton.disabled = true;
+}
+
+resetGame();
+
